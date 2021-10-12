0a666891c2efcf6cbfe866bd7bb5481a951dfdad
zjp <jiping_zhou@foxmail.com>
2021-10-12 20:47:55 +0800

    [rustdx-cmd] remove log related for now

--- rustdx-cmd/src/main.rs
+++ rustdx-cmd/src/main.rs
@@ -4,15 +4,15 @@ mod io;
 use anyhow::Result;
 
 fn main() -> Result<()> {
-    log_init()?;
+    // log_init()?;
     let cmd: cmd::TopLevel = argh::from_env();
     println!("{:?}", cmd);
     cmd.match_subcmd()
 }
 
-fn log_init() -> Result<()> {
-    use simplelog::{Config, LevelFilter, WriteLogger};
-    use std::fs::File;
-    let _ = WriteLogger::init(LevelFilter::Info, Config::default(), File::create("rustdx.log")?);
-    Ok(())
-}
+// fn log_init() -> Result<()> {
+//     use simplelog::{Config, LevelFilter, WriteLogger};
+//     use std::fs::File;
+//     let _ = WriteLogger::init(LevelFilter::Info, Config::default(),
+// File::create("rustdx.log")?);     Ok(())
+// }
