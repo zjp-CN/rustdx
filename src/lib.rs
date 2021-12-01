c2c20ad94cf992614c55333dbbf9550f880dc73b
zjp <jiping_zhou@foxmail.com>
2021-12-01 23:35:22 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/rustdx/0.2.4")]
+#![doc(html_root_url = "https://docs.rs/rustdx/0.2.5")]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 // #![feature(test)]
 // extern crate test;
