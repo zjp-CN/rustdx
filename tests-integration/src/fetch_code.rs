dfb6a5d068ab464dc5cc6e478f8c78d4c67ed565
zjp <jiping_zhou@foxmail.com>
2023-02-23 21:39:27 +0800

    test(fetch_code): add

--- tests-integration/src/fetch_code.rs
+++ tests-integration/src/fetch_code.rs
@@ -1,14 +1,17 @@
 #[test]
+#[ignore = "联网更新数据"]
 fn offical_stocks() {
-    use crate::{now, shot, snap};
+    use crate::{now, shot, snap, to_table};
     use rustdx_cmd::fetch_code::{offical_stocks, StockList};
 
-    shot!(now(), @"2023-02-23 15:50:16.053645411 +08:00");
+    shot!(now(), @"2023-02-23 16:28:04.861102493 +08:00");
 
     let mut set = StockList::with_capacity(6000);
     let (count, time) = elapse!(offical_stocks(&mut set).unwrap());
     let len = count.count();
-    assert_eq!(set.len(), len);
+    let set = set.into_iter().collect::<std::collections::BTreeSet<_>>();
+    shot!("SHSZ", to_table(&set));
+    assert_eq!(set.len(), len); // 似乎有时出现不相等
 
     snap!(count, @r###"
     SHSZ {
@@ -18,5 +21,5 @@ fn offical_stocks() {
     }
     "###);
     shot!(len, @"4798");
-    shot!(time, @"795");
+    shot!(time, @"894");
 }
